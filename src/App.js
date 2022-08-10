5d22388e4d8a1514c66e5d49a5e634de19397b98
Tamilselvan K <tamilselvanks007@gamil.com>
2022-08-10 08:41:08 +0530

    cta-btn🎉

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ContactBtn from "./js/components/ContactBtn/ContactBtn.jsx";
+import CTABtn from "./js/components/CTABtn/CTABtn.jsx";
 import Navbar from "./js/components/Navbar/Navbar.jsx";
 import PageHeader from "./js/components/PageHeader/PageHeader.jsx";
 import SkillsIntro from "./js/components/SkillsIntro/SkillsIntro.jsx";
@@ -10,6 +11,7 @@ function App() {
       <PageHeader />
       <SkillsIntro />
       <ContactBtn />
+      <CTABtn />
     </div>
   );
 }
---
+++ src/js/data/IntroInfo.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+export const IntroInfo = () => {
+  return (
+    <>
+      <p>
+        I'm Tamilselvan Kalimuthu Self taught Web developer with ability to
+        learn and collaborate in rapidly changing environments and compositions.{" "}
+        <br />
+        "I believe that the design process must always consider how people use
+        things in real life and lead to solutions that are clear, honest, and
+        genuinely useful"
+      </p>
+    </>
+  );
+};
+export const AboutInfo = () => {
+  const style = {
+    textDecoration: "underline",
+    fontWeight: "800",
+    color: "rgb(255, 245, 232)",
+  };
+  return (
+    <>
+      <p>
+        I'm Self-taught React.js/Front-end Developer.
+        <br />
+        <br />I build modern-day UI components to improve application
+        performance.
+        <br />
+        <br />I spend most of my time writing code for building User Interfaces.
+        I like enjoying working in the intersection of design and development. I
+        feel most productive when I'm able to design User Interfaces which not
+        only look good but has a good performance as well.
+      </p>
+    </>
+  );
+};
