384ce4c593daefa5fd7127db0c6ae39d433a3d22
Tamilselvan K <tamilselvanks007@gamil.com>
2022-08-12 09:19:40 +0530

    intro-about-info🎉

--- src/js/pages/Home/Home.jsx
+++ src/js/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import "../../../css/pages/Home/Home.css";
 import { motion } from "framer-motion";
 import Navbar from "../../components/Navbar/Navbar";
 import PageHeader from "../../components/PageHeader/PageHeader";
 import SkillsIntro from "../../components/SkillsIntro/SkillsIntro";
 import ContactBtn from "../../components/ContactBtn/ContactBtn";
-import "../../../css/pages/Home/Home.css";
+import Intro from "../../components/IntroText/Intro";
+import CTABtn from "../../components/CTABtn/CTABtn";
 
 const Home = () => {
   return (
@@ -24,6 +26,15 @@ const Home = () => {
         <div className="container">
           <section className="intro">
             <ContactBtn />
+            <Intro />
+            <div className="resume-btn-wrap">
+              <CTABtn
+                text="Resume"
+                link=""
+                padding="clamp(8px, 2px + 1vh, 10px)"
+                width="clamp(120px, 90px + 10vw, 220px)"
+              />
+            </div>
           </section>
         </div>
       </motion.div>
